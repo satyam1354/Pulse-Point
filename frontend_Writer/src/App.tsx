52e3c751362c93b6c0e7eabf4d0afce4cbb8fa28
Satyam19113 <Satyamkr1354@gmail.com>
2025-02-28 00:23:21 +0530

    created frontend for writer

---
+++ frontend_Writer/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        hellohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </div>
+    </>
+  )
+}
+
+export default App
